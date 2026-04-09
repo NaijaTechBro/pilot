4f2cae63624248c8f7bc2eb79daa120a99dcc282
NaijaTechBro <bakisodiq@gmail.com>
2026-04-09 22:40:57 +0100

    git tokenisation

--- headers/lexer.hpp
+++ headers/lexer.hpp
@@ -3,6 +3,21 @@
 #include <string>
 
 
+enum type {
+    TOKEN_ID,
+    TOKEN_INT,
+    TOKEN_EQUALS,
+    TOKEN_SEMICOLON,
+    TOKEN_LEFT_PAREN,
+    TOKEN_RIGHT_PAREN,
+};
+
+struct Token {
+    enum type TYPE;
+    std::string VALUE;
+
+};
+
 class Lexer
 {
     public:
@@ -11,9 +26,38 @@ class Lexer
         source = sourceCode;
         cursor = 0;
         size = sourceCode.length();
-        current = sourceCode.at(cursor);
+
+        // Safety check: only call .at() if the string isn't empty!
+        if (size > 0) {
+            current = source.at(cursor);
+        } else {
+            current = '\0';
+        }
     }
 
+    char advance ()
+    {
+        if (cursor < size) {
+            char temp = current;
+            cursor++;
+            current = (cursor < size ) ? source[cursor] : '\0';
+
+            return temp;
+        }
+        else {
+            return '\0';
+        }
+    }
+
+    char peak (int offset = 0)
+    {
+        if (cursor + offset < size) {
+            return source[cursor + offset];
+        }
+        else {
+            return '\0';
+        }
+    }
     private:
     std::string source;
     int cursor;
--- pilot.cpp
+++ pilot.cpp
@@ -16,16 +16,16 @@ int main(int argc, char ** argv) {
     std::cout << "Reading from the file name : " << argv[1] << std::endl;
     std::ifstream sourceFileStream (argv[1]);
 
-    std::stringstream buffer;
-
-    char temp;
-
-    while (sourceFileStream.get(temp))
-    {
-        buffer << temp;
+    if (!sourceFileStream.is_open()) {
+        std::cout << "Could not open file: " << argv[1] << std::endl;
+        exit(1);
     }
 
+    std::stringstream buffer;
+    buffer << sourceFileStream.rdbuf();
     std::string sourceCode = buffer.str();
+
+    char temp;
 
     std::cout << "This is the source code " << std::endl << sourceCode;
 
